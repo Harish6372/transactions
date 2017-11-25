beca1f3b779a98716c4f3d2f5d4c3020c09c68b1
sureharish <sureharish63@gmail.com>
2017-11-25 17:27:21 +0530

    added all java files

---
+++ src/main/java/com/example/HarishSpringTransactionDemo/HarishSpringTransactionDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.HarishSpringTransactionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarishSpringTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarishSpringTransactionDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/HarishSpringTransactionDemo/HarishSpringTransactionDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.HarishSpringTransactionDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HarishSpringTransactionDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
